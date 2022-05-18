d50e4d21c0a756dbcf86dfba5bbef1b641bced1c
hemantahire002 <93426308+hemantahire002@users.noreply.github.com>
2022-05-18 22:12:14 +0530

    change

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -2,10 +2,10 @@ import React, { Fragment, useEffect } from "react";
 import { CgMouse } from "react-icons/all";
 import "./Home.css";
 import ProductCard from "./ProductCard.js";
-import MetaData from "../layout/MetaData";
+
 import { clearErrors, getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
-import Loader from "../layout/Loader/Loader";
+
 import { useAlert } from "react-alert";
 
 const Home = () => {
